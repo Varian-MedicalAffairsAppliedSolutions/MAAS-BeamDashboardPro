ad8e7031d7d7925dcacbf73b4a722577778916a1
Michael Folkerts <Michael.Folkerts@varian.com>
2023-06-29 06:00:36 +0000

    added missing hidden imports for pyinstaller

--- streamlit_runner.py
+++ streamlit_runner.py
@@ -1,9 +1,6 @@
 import streamlit.web.cli as stcli
 import sys
 
-# the following are needed for PyInstaller:
-import streamlit.runtime.scriptrunner.magic_funcs
-
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
@@ -13,7 +10,7 @@ if __name__ == "__main__":
     script_path = sys.argv[1]  # first argument should be the path to the streamlit script
     script_args = sys.argv[2:]  # additional arguments are passed along to the streamlit script
     
-    # monkey-patch sys arguments to be picked up by streamlit main
+    # patch sys arguments to be picked up by streamlit main
     sys.argv = [
         "streamlit",
         "run",
