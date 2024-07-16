39f65294775465805b074b7493ea12f928c070e6
Michael Folkerts <michael.folkerts@varian.com>
2024-07-15 18:15:46 -0700

    Update streamlit_runner.py
    formatting

--- streamlit_runner.py
+++ streamlit_runner.py
@@ -5,7 +5,8 @@ from expiration import EXP_DATE
 
 if __name__ == "__main__":
     print("This software is valid until " + EXP_DATE.isoformat())
-    print("Check here for the latest releases - https://github.com/Varian-MedicalAffairsAppliedSolutions/MAAS-BeamDashboardPro/releases")
+    print("Check here for the latest releases:")
+    print("https://github.com/Varian-MedicalAffairsAppliedSolutions/MAAS-BeamDashboardPro/releases")
 
     if(datetime.today() > EXP_DATE):
         print("[ERROR] This software has expired.")
